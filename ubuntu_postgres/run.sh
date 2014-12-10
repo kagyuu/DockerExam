054c35546e430a03cc457ba7a260392fab0180d7
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2014-12-11 02:13:12 +0900

    add rsyslog

--- ubuntu_postgres/run.sh
+++ ubuntu_postgres/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#docker run -p 10022:22 -p 12812:2812 -p 5432:5432 \
+#--volumes-from=naughty_einstein \
+#-d atsushi/ubuntu_postgresql94b2
+
 docker run -p 10022:22 -p 12812:2812 -p 5432:5432 \
---volumes-from=naughty_einstein \
 -d atsushi/ubuntu_postgresql94b2
