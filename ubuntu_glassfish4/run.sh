eb2e23cfd5df44ebea18c3fb62d117924b746fe8
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2014-12-15 02:44:12 +0900

    fix gs admin console

--- ubuntu_glassfish4/run.sh
+++ ubuntu_glassfish4/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 10022:22 -p 12812:2812 -p 14343:4343 -p 18080:8080 -d atsushi/ubuntu_glassfish41
+docker run -p 10022:22 -p 12812:2812 -p 14848:4848 -p 18080:8080 -d atsushi/ubuntu_glassfish41
