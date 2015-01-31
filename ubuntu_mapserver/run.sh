b67c99e73c665d0fec9f1ebf17139eed830ab9db
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2015-01-31 13:09:12 +0900

    add EPSG:3857 projectio for mapserver example

--- ubuntu_mapserver/run.sh
+++ ubuntu_mapserver/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -p 10022:22 -p 12812:2812 -p 5432:5432 -p 10080:80 \
--v /home/core/git/DockerExam/ubuntu_mapserver/maps:/opt/maps:rw \
--v /home/core/git/DockerExam/ubuntu_mapserver/html:/var/www/html:rw \
+-v /home/atsushi/git/DockerExam/ubuntu_mapserver/maps:/opt/maps:rw \
+-v /home/atsushi/git/DockerExam/ubuntu_mapserver/html:/var/www/html:rw \
 -d atsushi/ubuntu_mapserver
