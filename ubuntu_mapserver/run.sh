779fcd32c46468a776a0c3ba20acc56863dfdafb
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2015-02-01 22:36:44 +0900

    add mapcache for mapserver

--- ubuntu_mapserver/run.sh
+++ ubuntu_mapserver/run.sh
@@ -2,5 +2,6 @@
 
 docker run -p 10022:22 -p 12812:2812 -p 5432:5432 -p 10080:80 \
 -v /home/atsushi/git/DockerExam/ubuntu_mapserver/maps:/opt/maps:rw \
+-v /home/atsushi/git/DockerExam/ubuntu_mapserver/mapcache:/opt/mapcache:rw \
 -v /home/atsushi/git/DockerExam/ubuntu_mapserver/html:/var/www/html:rw \
 -d atsushi/ubuntu_mapserver
